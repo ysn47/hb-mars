adc00ea89476742acf2ccf60e8ecf774c3848def
Yasin Demir <yasin.demir@synapsis.com.tr>
2020-10-17 19:07:20 +0300

    add some tests

--- src/MARS.Tests/RoverTests.cs
+++ src/MARS.Tests/RoverTests.cs
@@ -23,5 +23,29 @@ namespace MARS.Tests
 
             Assert.Equal(rover.GetCurrentPositionAndDirection(), expected);
         }
+
+        [Theory]
+        [InlineData("1 1", "0 0 N", "MMM")]
+        public void Out_Of_Plateau_Should_Throw_Exception(string plateauPosition, string roverPosition, string commands)
+        {
+            var plateauPoints = plateauPosition.Split(' ').Select(p => Convert.ToInt32(p)).ToArray();
+            IPlateau plateau = new Plateau(plateauPoints[0], plateauPoints[1]);
+
+            var roverData = roverPosition.Split(' ');
+            IRover rover = new Rover(plateau, Convert.ToInt32(roverData[0]), Convert.ToInt32(roverData[1]), (Direction)Enum.Parse(typeof(Direction), roverData[2]));
+            Assert.Throws<ArgumentOutOfRangeException>(() => rover.ApplyCommandBulk(commands));
+        }
+
+        [Theory]
+        [InlineData("5 5", "0 0 N", "S")]
+        public void Invalid_Command_Should_Throw_Exception(string plateauPosition, string roverPosition, string commands)
+        {
+            var plateauPoints = plateauPosition.Split(' ').Select(p => Convert.ToInt32(p)).ToArray();
+            IPlateau plateau = new Plateau(plateauPoints[0], plateauPoints[1]);
+
+            var roverData = roverPosition.Split(' ');
+            IRover rover = new Rover(plateau, Convert.ToInt32(roverData[0]), Convert.ToInt32(roverData[1]), (Direction)Enum.Parse(typeof(Direction), roverData[2]));
+            Assert.Throws<InvalidOperationException>(() => rover.ApplyCommandBulk(commands));
+        }
     }
 }
